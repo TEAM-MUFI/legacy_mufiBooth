9f4d02bb070b233aefa1afd9d5f3b775c5872fde
TheLifeOfSeo <dsa3182@gmail.com>
2022-09-17 21:45:10 +0900

    SEO : upload files

---
+++ static/js/popup.js
@@ -0,0 +1,45 @@
+// const btnTest = document.querySelector("#visible-test")
+const pop = document.querySelector("#pop")
+const popArea = document.querySelector("#pop-screen");
+const popClose = document.querySelector("#pop-close");
+const popPrice = document.querySelector("#pop-price");
+// btnTest.addEventListener("click", visible)
+
+
+
+
+function test() {
+    console.log("clicked");
+}
+
+
+
+function visible() {
+    document.body.style.overflow = 'hidden';
+    pop.classList.remove("hidden")
+    console.log("hihi");
+}
+
+
+function hidden() {
+    document.body.style.overflow = 'auto';
+    pop.classList.add("hidden");
+    console.log("hi")
+}
+
+
+popClose.addEventListener("click", hidden);
+
+
+
+window.addEventListener("click", (event) => {
+    event.target === pop ? hidden() : false;
+})
+
+
+
+// 모달 on
+// body.style.overflow = 'hidden';
+
+// 모달 off
+// body.style.overflow = 'auto';
