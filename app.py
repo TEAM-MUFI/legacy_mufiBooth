33e665ab88e55fb8fb71570c4352430036b0aa07
TheLifeOfSeo <dsa3182@gmail.com>
2022-09-17 22:29:42 +0900

    fix error

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 # 첫 번째 Flask Server
 from flask import Flask, render_template
 from flask_restx import Api, Resource  # Api 구현을 위한 Api 객체 import
-# from kisok.mufiKiosk import kiosk
-# from web.mufiWeb import mufiweb
+from kisok.mufiKiosk import kiosk
+from web.mufiWeb import mufiweb
 
 app = Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌.
 api = Api(app)  # Flask 객체에 Api 객체 등록
@@ -12,7 +12,7 @@ api = Api(app)  # Flask 객체에 Api 객체 등록
 
 # api.add_namespace(web,'/web')
 
-# api.add_namespace(page,'/page')
+api.add_namespace(page,'/page')
 
 
 
