9ce1864ede73b1ccba2e84d95e270b398e2ba9ee
TheLifeOfSeo <dsa3182@gmail.com>
2022-09-19 00:23:27 +0900

    add links / add layouts

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,51 +1,65 @@
 let menu = [
     {
-        name: "서원대학교",
+            name: "가장 많이 찾는 사진",
+            frame: "",
+            size: 4,
+            price: 3000,
+            img: "Seowon-4.png"
+    },
+    {
+        name: "아늑하고 우아한 사진",
         frame: "",
-        size: 4,
-        price: 3000,
-        img: "Seowon-4.png"
-    },
-    {
-        name: "아늑하고 우아한 사진",
-        frame: "충북대학교",
-        size: 6,
-        price: 4000,
-        img: "CBNU-6-1.png"
-    },
-    {
-        name: "아늑하고 우아한 사진",
-        frame: "충북대학교",
         size: 6,
         price: 4000,
         img: "CBNU-6-2.png"
     },
-    {
-        name: "아늑하고 우아한 사진",
-        frame: "충북대학교",
-        size: 6,
-        price: 4000,
-        img: "CBNU-6-3.png"
-    },
-    {
-        name: "아늑하고 우아한 사진",
-        frame: "충북대학교",
-        size: 6,
-        price: 4000,
-        img: "CBNU-6-4.png"
-    },
-    {
-        name: "아늑하고 우아한 사진",
-        frame: "충북대학교",
-        size: 6,
-        price: 4000,
-        img: "CBNU-6-5.png"
-    },
-    {
-        name: "아늑하고 우아한 사진",
-        frame: "충북대학교",
-        size: 6,
-        price: 4000,
-        img: "CBNU-6-6.png"
-    }
+    // {
+    //     name: "서원대학교",
+    //     frame: "",
+    //     size: 4,
+    //     price: 3000,
+    //     img: "Seowon-4.png"
+    // },
+    // {
+    //     name: "아늑하고 우아한 사진",
+    //     frame: "충북대학교",
+    //     size: 6,
+    //     price: 4000,
+    //     img: "CBNU-6-1.png"
+    // },
+    // {
+    //     name: "아늑하고 우아한 사진",
+    //     frame: "충북대학교",
+    //     size: 6,
+    //     price: 4000,
+    //     img: "CBNU-6-2.png"
+    // },
+    // {
+    //     name: "아늑하고 우아한 사진",
+    //     frame: "충북대학교",
+    //     size: 6,
+    //     price: 4000,
+    //     img: "CBNU-6-3.png"
+    // },
+    // {
+    //     name: "아늑하고 우아한 사진",
+    //     frame: "충북대학교",
+    //     size: 6,
+    //     price: 4000,
+    //     img: "CBNU-6-4.png"
+    // },
+    // {
+    //     name: "아늑하고 우아한 사진",
+    //     frame: "충북대학교",
+    //     size: 6,
+    //     price: 4000,
+    //     img: "CBNU-6-5.png"
+    // },
+    // {
+    //     name: "아늑하고 우아한 사진",
+    //     frame: "충북대학교",
+    //     size: 6,
+    //     price: 4000,
+    //     img: "CBNU-6-6.png"
+    // }
 ]
--- web/mufiWeb.py
+++ web/mufiWeb.py
@@ -1,4 +1,4 @@
-from flask import request, render_template, send_file, make_response, send_from_directory
+from flask import request, render_template, send_file, make_response, send_from_directory, redirect, url_for
 from flask_restx import Resource, Api, Namespace
 from werkzeug.serving import WSGIRequestHandler
 
@@ -15,22 +15,22 @@ class webRender(Resource):
     def get(self, file):
         return send_file("./static/img/frame/" + file)
 
-<<<<<<< HEAD
-@web.route('/frame/<string:pin>')
-class webRender(Resource):
-    def get(self, pin):
-        return send_file("./static/img/frame/" + file)
-=======
-# @web.route('/frame/<string:핀번호>')
+# @web.route('/frame/<string:pin>')
 # class webRender(Resource):
-    # def get(self, file):
+    # def get(self, pin):
         # return send_file("./static/img/frame/" + file)
->>>>>>> b0548a7a0f91b7399fc170b36de31d5ed968837d
 
 @web.route('/signin') #로그인페이지
 class webRender(Resource):
     def get(self):
         return make_response(render_template('signin.html'))
+
+# @web.route('/signin/<><><>', methods=['GET']) #로그인성공후
+# class webRender(Resource):
+#     def get(self):
+#         # code_receive = request.args.get('name_give')
+#         print(request.args)
+#         return redirect(f'/webserver/oauth')
 
 @web.route('/select') #사진 촬영/조회 선택 페이지
 class webRender(Resource):
@@ -50,9 +50,6 @@ class webRender(Resource):
 @web.route('/pin') #핀 번호 발급 페이지
 class webRender(Resource):
     def get(self):
-<<<<<<< HEAD
-        return make_response(render_template('new-5.html'))
-=======
         return make_response(render_template('pin.html'))
 
 @web.route('/fail') #결제 실패 페이지
@@ -64,4 +61,3 @@ class webRender(Resource):
 class webRender(Resource):
     def get(self):
         return make_response(render_template('photo.html'))
->>>>>>> b0548a7a0f91b7399fc170b36de31d5ed968837d
