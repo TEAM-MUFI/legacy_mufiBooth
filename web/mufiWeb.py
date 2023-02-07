4c52e692e21bbfde62b5c19ad4d861e14c9f3fa5
JungHoon99 <greenjm1@nate.com>
2023-02-07 09:21:48 +0000

    didn't use code delete

--- web/mufiWeb.py
+++ web/mufiWeb.py
@@ -5,30 +5,26 @@ from werkzeug.serving import WSGIRequestHandler
 web = Namespace('web')
 
 WSGIRequestHandler.protoco_version = "HTTP/1.1"
-@web.route('')
-class webPost(Resource):
-    def post(self):
-        return {'hello':'world'}
+
+
 
 @web.route('/frame/<string:file>')
 class webRender(Resource):
     def get(self, file):
         return send_file("./static/img/frame/" + file)
 
-# @web.route('/frame/<string:pin>')
-# class webRender(Resource):
-    # def get(self, pin):
-        # return send_file("./static/img/frame/" + file)
 
 @web.route('/signin') #로그인페이지
 class webRender(Resource):
     def get(self):
         return make_response(render_template('signin.html'))
 
+
 @web.route('/picture/<string:name>')
 class picture(Resource):
     def get(self,name):
         return send_file("./picture/"+name)
+
 
 @web.route('/subphoto/<string:name>')
 class subphoto(Resource):
