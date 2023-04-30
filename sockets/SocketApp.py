7924359487224fe10e07a438696ce6e1a35d17bb
JungHoon99 <greenjm1@nate.com>
2023-04-30 14:25:30 +0000

    socketio protocol test

--- sockets/SocketApp.py
+++ sockets/SocketApp.py
@@ -12,6 +12,7 @@ def on_connect():
 @socketio.on('join_room')
 def on_join_room(data):
     join_room(data)
+    emit('send_jwt', {'jwt': "sa3EW4rfw4GdVDres63e3bsdG5"}, room=data)
 
 @socketio.on('leave_room')
 def on_leave_room(data):
@@ -20,6 +21,7 @@ def on_leave_room(data):
 @socketio.on('close_room')
 def on_close_room(data):
     close_room(data)
+    emit('access_close',"success")
 
 @socketio.on('disconnect')
 def on_disconnect():
@@ -27,10 +29,10 @@ def on_disconnect():
 
 @socketio.on('message')
 def on_message(data):
-    emit('receive_message', data)
+    emit('receive_message', data+"receive")
 
 @socketio.on('room_message')
 def on_room_message(data):
     room = data['room']
     message = data['message']
-    emit('message', {'message': message}, room=room)
+    emit('test_message', {'message': "receive message : " + message}, room=room)
